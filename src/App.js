9f3e9e51cf44c28b68e4f2e9cf3ee04cccf9808b
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-05-27 16:05:56 +0500

    Add UserParticipation Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import PollDispaly from './Components/PollDispaly';
+import UserParticipation from './Components/UserParticipation';
+
 function App() {
 
   return (
     <div className='d-flex flex-column justify-content-center align-items-center'>
+      
       <h1 className='text-center'>Polling App</h1>
       <PollDispaly />
-      
+      <UserParticipation />
+
     </div>
     );
 }
--- src/Components/UserParticipation.jsx
+++ src/Components/UserParticipation.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 export default function UserParticipation() {
-  return (
-    <div>UserParticipation</div>
+  
+    const formik = useFormik({
+        initialValues: {
+            choice: ''
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+  
+    return (
+    <div>
+        <form action="" onSubmit={formik.handleSubmit}>
+            <h1>Answer Choices:</h1>
+            <div role='group' onChange={formik.handleChange}>
+                <label htmlFor=""><input type="radio" name='choice' value={"javascript"}/> JavaScript</label>
+                <br />
+                <label htmlFor=""><input type="radio" name='choice' value={"python"}/> Python</label>
+                <br />
+                <label htmlFor=""><input type="radio" name='choice' value={"java"}/> Java</label>
+                <br />
+                <label htmlFor=""><input type="radio" name='choice' value={"csharp"}/> C#</label>
+                <br />
+                <button type='submit'>Submit vote</button>
+            </div>
+        </form>
+        
+    </div>
   )
 }
