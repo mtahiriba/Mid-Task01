15994d9cc99657343b8c7ee402db84eb2b0d453d
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-05-27 15:26:20 +0500

    Initial Hello world! project uploaded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <h1 className='d-flex justify-content-center align-items-center bg-primary text-white'>
+      Hello world!
+    </h1>
+    );
+}
+
+export default App;
